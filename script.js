47c4462e22a3263833ac7247d79afe824a34c500
abhi-2605 <abhishekmenon2605@gmail.com>
2021-02-10 21:52:37 +0530

    edited7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("button").click(function() {
+    $("#button").click(function() {
 
         var key = $("#key").val();
         var kk = document.getElementById("key");
@@ -22,5 +22,28 @@ function preventBack() {
 }
 setTimeout("preventBack()", 0);
 window.onunload = function() {
-    null;
-}
+        null;
+    }
+    // Wrap every letter in a span
+
+
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml7 .letter',
+        translateY: ["1.1em", 0],
+        translateX: ["0.55em", 0],
+        translateZ: 0,
+        rotateZ: [180, 0],
+        duration: 6600,
+        easing: "easeOutExpo",
+        delay: (el, i) => 50 * i
+    }).add({
+        targets: '.ml7',
+        opacity: 1,
+        duration: 20000,
+        easing: "easeOutExpo",
+        delay: 100000
+    });
